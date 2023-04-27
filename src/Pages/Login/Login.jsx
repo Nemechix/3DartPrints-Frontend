b50988b2272124c6825f5554ed1926ae174f86a0
Crayyarc <crayyarc@gmail.com>
2023-04-27 12:16:13 +0100

    add Login

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginFrame from '../../Components/LoginFrame/LoginFrame'
+
+
+
+function Login() {
+  return (
+      <LoginFrame />
+  )
+}
+
+export default Login
