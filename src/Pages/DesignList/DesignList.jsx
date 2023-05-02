616aa0ce04ed3fadcb614e6033aa2d2f5b04960a
Crayyarc <122812033+Crayyarc@users.noreply.github.com>
2023-05-02 14:52:04 +0100

    Create DesignList.jsx

---
+++ src/Pages/DesignList/DesignList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DesignListFrame from '../../Components/DesignListFrame/DesignListFrame.jsx/DesignListFrame'
+
+function DesignList() {
+  return (
+      <DesignListFrame />
+  )
+}
+
+export default DesignList;
