999c393c999c09999d4d39535c422d62316353de
Crayyarc <122812033+Crayyarc@users.noreply.github.com>
2023-05-02 17:03:47 +0100

    update cart frame

--- src/Components/CartFrame/CartFrame.jsx
+++ src/Components/CartFrame/CartFrame.jsx
@@ -3,10 +3,15 @@ import './CartFrame.css';
 
 function CartFrame() {
   const [cartItems, setCartItems] = useState([]);
+  }
 
-  const addToCart = (product) => {
-    setCartItems([...cartItems, product]);
-  };
+  const handleAddToCart = (design) => {
+    const product = {
+      id: cartItems.length + 1,
+      name: '3D Model',
+      price,
+      printer,
+    };
 
   const removeFromCart = (product) => {
     const newCartItems = cartItems.filter((item) => item !== product);
@@ -21,25 +26,25 @@ function CartFrame() {
         <p>Tu carrito esta vacio.</p>
       ) : (
         <ul>
-          {cartItems.map((item) => (
-            <li key={item.id}>
-              {item.name} - {item.price}{' '}
-              <button onClick={() => removeFromCart(item)}>Borrar</button>
-
+          {cartItems.map((design) => (
+            <li key={design.id}>
+              {design.name} - {design.price}{' '}
+              {user.printer && <span> - Printed on {user.printer}</span>}
+              <button onClick={() => removeFromCart(design)}>Remove</button>
             </li>
           ))}
         </ul>
       )}
-      <button onClick={() => addToCart(item)}>Añadir</button>
+      <button onClick={() => handleAddToCart(design)}>Añadir</button>
       <hr />
       <div>
       <br></br>
       </div>
-      <p>Total: {cartItems.reduce((acc, item) => acc + item.price, 0)}</p>
-      <button>Pagar</button>
+      <p>Total: {cartItems.reduce((acc, design) => acc + design.price, 0)}</p>
+      <button>Checkout</button>
       </form>
     </div>
   );
-}
+
 
 export default CartFrame;
