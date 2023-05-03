0d54949f8f9783b345c540db3975076a1c25211a
Nemechix <nemesi.jobs@gmail.com>
2023-05-03 10:21:10 +0000

    page for designs

--- src/Pages/UniqueDesign/UniqueDesign.jsx
+++ src/Pages/UniqueDesign/UniqueDesign.jsx
@@ -1,26 +1,31 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import  getDesignById  from '../../Services/getDesignById';
+import getDesignById from '../../Services/getDesignById';
 
 function UniqueDesign() {
-    const [design, setDesign ] = useState({})
-    const {id} = useParams()
-    
-    
-      const getDesign = async () => {
-        const result = await getDesignById(id);
-        setDesign(result.data);
-      };
-    
-      useEffect(() => {
-        getDesign();
-      }, [id]);
-    
+  const [design, setDesign ] = useState({});
+  const {id} = useParams();
+
+  const getDesign = async () => {
+    const result = await getDesignById(id);
+    setDesign(result.data);
+  };
+
+  useEffect(() => {
+    getDesign();
+  }, [id]);
+
+  const addToCart = () => {
+    console.log('Artículo agregado al carrito');
+  };
 
   return (
-    <div>
-      <h1>{design.name}</h1>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <img style={{paddingTop: '20px', width: '80vw', borderRadius: '10px'}} src={design.image} alt={design.name} />
+      <h1 style={{marginTop: '20px'}}>{design.price}€</h1>
+      <h3>{design.name}</h3>
       <p>{design.description}</p>
+      <button style={{marginTop: '20px',marginBottom:"15px"}} onClick={addToCart}>Añadir al carrito</button>
     </div>
   );
 }
--- src/Services/getDesignById.jsx
+++ src/Services/getDesignById.jsx
@@ -1,8 +1,6 @@
 import api from "./config";
 
 async function getDesignById(id) {
-  console.log(id)
-  console.log("hola")
   const design = await api.get(`design/${id}`);
   return design;
 }
