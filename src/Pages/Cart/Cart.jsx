9228459a0012a420f6d1df682bb61f7c269c2c44
Crayyarc <122812033+Crayyarc@users.noreply.github.com>
2023-05-03 14:10:07 +0100

    add checkout

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import CartFrame from '../../Components/CartFrame/CartFrame'
+import Checkout from '../../Components/Checkout/Checkout'
 
   function Cart() {
     return (
       <div>
         <CartFrame />
+        <Checkout />
       </div>
     );
   }
