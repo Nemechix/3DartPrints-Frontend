97c0dc812a354a09e63d28413755d62a485f6810
Crayyarc <122812033+Crayyarc@users.noreply.github.com>
2023-05-03 13:22:48 +0100

    fix

--- src/Components/CartFrame/CartFrame.jsx
+++ src/Components/CartFrame/CartFrame.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './CartFrame.css';
-import  baseURL  from '../../Services/config';
+import { baseURL } from '../../Services/config';
 
 function CartFrame() {
   const [cartItems, setCartItems] = useState([]);
@@ -8,11 +8,16 @@ function CartFrame() {
   const [price, setPrice] = useState('');
 
   const handleAddToCart = async (design) => {
-    // Hacer una llamada a la API para obtener la información del diseño
+    //llama la API para obtener la información del diseño
     const response = await fetch(`${baseURL}/design/${design.id}`);
     const designData = await response.json(); 
+
+    // llamada a API = info de la impresora del usuario
+    const userResponse = await fetch(`${baseURL}/users/${userId}`);
+    const userData = await userResponse.json();
+    const printer = userData.printer;
   
-    // objeto producto, infor + diseño + precio
+    // objeto producto, info + diseño + precio + impresora
     const product = {
       id: cartItems.length + 1,
       name: design.name,
@@ -21,17 +26,17 @@ function CartFrame() {
       designId: design.id,
     };    
   
-    // Añadir producto al carrito
+    // añadir producto al carrito
     setCartItems([...cartItems, product]);
   };
   
-   // Elimina producto carrito
+   // elimina producto carrito
   const removeFromCart = (product) => {
     const newCartItems = cartItems.filter((item) => item !== product);
     setCartItems(newCartItems);
   };
   
-    return (
+  return (
     <div className="cart-wrapper">
       <form>
         <h2>Carrito de compra:</h2>
@@ -42,8 +47,7 @@ function CartFrame() {
           <ul>
             {cartItems.map((design) => (
               <li key={design.id}>
-                {design.name} - {design.price} {' '}
-                {design.printer && <span> - Printed on {design.printer}</span>}
+                {design.name} - {design.price} {design.printer && <span> - Printed on {design.printer}</span>}
                 <button onClick={() => removeFromCart(design)}>Remove</button>
               </li>
             ))}
@@ -51,12 +55,12 @@ function CartFrame() {
         )}
         <label>
           Precio:
-          <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+          <input type="text" value={design.price} onChange={(e) => setDesign({ ...design, price: e.target.value })} />
         </label>
         <br />
         <label>
-          Printer:
-          <input type="text" value={printer} onChange={(e) => setPrinter(e.target.value)} />
+          Impresora:
+          <input type="text" value={design.printer} onChange={(e) => setDesign({ ...design, printer: e.target.value })} />
         </label>
         <br />
         <button onClick={() => handleAddToCart(design)}>Añadir</button>
