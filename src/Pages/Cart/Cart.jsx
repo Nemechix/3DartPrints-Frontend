bdead748d898b9692330fa9c350268a988b5dd00
Crayyarc <122812033+Crayyarc@users.noreply.github.com>
2023-05-04 10:17:27 +0100

    Update Cart.jsx

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { baseURL } from '../../Services/config';
+import { useState, useEffect } from 'react';
 import Payment from '../../Components/Payment/Payment';
 import CartFrame from '../../Components/CartFrame/CartFrame'
+import { getCartTotal } from '../../services/cartService';
 
   function Cart() {
-    const [cartItems, setCartItems] = useState([]);
+    // const [cartItems, setCartItems] = useState([]);
     const [cartTotal, setCartTotal] = useState(0);
 
     useEffect(() => {
       const fetchCartTotal = async () => {
-        const response = await fetch(`${baseURL}/cartTotal`);
-        const data = await response.json();
-        setCartTotal(data.total);
+        const total = await getCartTotal();
+        setCartTotal(total);
       };
       fetchCartTotal();
     }, []);
