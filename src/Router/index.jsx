67859539bafb5c2b5f41a104fe5bee1e653d897b
Crayyarc <122812033+Crayyarc@users.noreply.github.com>
2023-05-08 15:51:32 +0100

    Update index.jsx

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -9,7 +9,7 @@ import UserByUsername from "../Pages/UserById/UserByUsername";
 import DesignsByCategory from "../Pages/DesingsByCategory/DesignsByCategory";
 import UniqueDesign from "../Pages/UniqueDesign/UniqueDesign";
 import CartUser from "../Pages/CartUser/CartUser";
-import PrivateRoute from "../../Components/PrivateRoute/PrivateRoute";
+import PrivateRoute from "../Components/PrivateRoute/PrivateRoute";
 import AdminTools from "../Pages/AdminTools/AdminTools";
 
 const router = createBrowserRouter([
