99f3cf98eed028dadf605b7a52e776c55edf288b
Crayyarc <122812033+Crayyarc@users.noreply.github.com>
2023-05-08 15:40:51 +0100

    Create PrivateRoute.jsx

---
+++ src/Components/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,25 @@
+import { Route, Redirect } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import { useSelector } from "react-redux";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const token = localStorage.getItem("token");
+  const user = token ? jwt_decode(token) : null;
+  const isAdmin = user ? user.isAdmin : false;
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated && isAdmin ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
