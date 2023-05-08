fe247dd22053df3e23e413e5d3bb432e3462a5e9
Crayyarc <122812033+Crayyarc@users.noreply.github.com>
2023-05-08 15:55:11 +0100

    Create AdminTools.jsx

---
+++ src/Pages/AdminTools/AdminTools.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+
+const AdminTools = () => {
+  const [loggedIn, setLoggedIn] = useState(true);
+  if (!loggedIn) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div>
+      <h1>Admin Tools v1.0 - 3DartPrints</h1>
+      <p> </p>
+    </div>
+  );
+};
+
+export default AdminTools;
