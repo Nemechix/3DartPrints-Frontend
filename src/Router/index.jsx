98b93c137bf868de54012c928b7784af0a3ba174
Nemechix <nemesi.jobs@gmail.com>
2023-05-09 13:38:34 +0100

    errores comentados

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -9,9 +9,9 @@ import UserByUsername from "../Pages/UserById/UserByUsername";
 import DesignsByCategory from "../Pages/DesingsByCategory/DesignsByCategory";
 import UniqueDesign from "../Pages/UniqueDesign/UniqueDesign";
 import CartUser from "../Pages/CartUser/CartUser";
-import PrivateRoute from "../Components/PrivateRoute/PrivateRoute";
+//import PrivateRoute from "../Components/PrivateRoute/PrivateRoute";
 //import AdminTools from "../Pages/AdminTools/AdminTools";
-import PrinterModelsList from "../Pages/PrinterModelList/PrinterModelList";
+//import PrinterModelsList from "../Pages/PrinterModelList/PrinterModelList";
 
 const router = createBrowserRouter([
   {
@@ -57,11 +57,11 @@ const router = createBrowserRouter([
       {
         path: "/user/cart",
         element: <CartUser />,
-      },
+      }/* ,
       {
         path: "/printer",
         element: <PrinterModelList />,
-      },
+      }, */
     ],
   },
 ]);
