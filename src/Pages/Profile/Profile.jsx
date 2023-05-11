65ad7e4ec695dc3493b04eef8fd0f858a5edd292
DaniSuarezG <DanielSuarezGalvez@Gmail.com>
2023-05-11 17:11:12 +0100

    Profile comment because fails

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,32 +1,32 @@
-import { useState, useEffect } from "react";
-import { Card, CardActions, CardContent, CardHeader, Divider, TextField, Typography } from '@mui/material';
-import Button3D from "../../Components/Button/Button";
+// import { useState, useEffect } from "react";
+// import { Card, CardActions, CardContent, CardHeader, Divider, TextField, Typography } from '@mui/material';
+// import Button3D from "../../Components/Button/Button";
 
-function Profile() {
+// function Profile() {
 
-      };
+//       };
       
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-  };
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//   };
 
-  return (
-    <div className="register-wrapper">
-  <Card>
-      <CardHeader title="Profile"></CardHeader>
-        <CardContent onSubmit={handleSubmit}>
-                <TextField
-                    label="Nombre:"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleInputChange}
-                    fullWidth={true}
-                    variant="outlined"
-                    margin="dense"
-    />
-        </CardContent>
-  </Card>
-    </div>
-      )
+//   return (
+//     <div className="register-wrapper">
+//   <Card>
+//       <CardHeader title="Profile"></CardHeader>
+//         <CardContent onSubmit={handleSubmit}>
+//                 <TextField
+//                     label="Nombre:"
+//                     name="name"
+//                     value={formData.name}
+//                     onChange={handleInputChange}
+//                     fullWidth={true}
+//                     variant="outlined"
+//                     margin="dense"
+//     />
+//         </CardContent>
+//   </Card>
+//     </div>
+//       )
 
-export default Profile;
+// export default Profile;
