2290986cb44eb2f6c2860ce7afd5913dca14d559
DaniSuarezG <danielsuarezgalvez@gmail.com>
2023-05-13 19:34:33 +0100

    SERVICES - Backend URL

--- src/Services/config.jsx
+++ src/Services/config.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: import.meta.env.VITE_TEST
-  //baseURL: 'https://threedartprints-2yqk.onrender.com/api'
-  baseURL: 'http://localhost:3000/api'
+  baseURL: 'https://threedartprints-2yqk.onrender.com/api'
+  // baseURL: 'http://localhost:3000/api'
 })
 
 export default api
